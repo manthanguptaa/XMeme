f8cfae63cbd22ec044731fb3cb1ccb153b155232
Manthan Gupta <manthangupta109@gmail.com>
2021-02-06 00:09:13 +0530

    get request to get all the memes

---
+++ model/meme.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const memeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    caption: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('meme', memeSchema)
--- routes/memes.js
+++ routes/memes.js
@@ -1,14 +1,21 @@
 //3rd party libraries
 const express = require('express')
 
+const meme = require('../model/meme')
+
 const router = express.Router()
 
-router.get('/', async(req, res , next)=>{
-    try{
-        const memes = 
-    }catch(err){
-        res.send('Error '+err)
+router.get('/', async (req, res, next) => {
+    try {
+        const memes = await meme.find()
+        res.json(memes)
+    } catch (err) {
+        res.send('Error ' + err)
     }
+})
+
+router.post('/', async (req, res, next) => {
+    
 })
 
 module.exports = router
