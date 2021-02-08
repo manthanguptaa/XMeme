b7154db30e90fd1dccab91d3e3018799fc1d2511
Manthan Gupta <manthangupta109@gmail.com>
2021-02-09 01:39:32 +0530

    Swagger api documentation endpoints added

--- routes/views.js
+++ routes/views.js
@@ -10,9 +10,33 @@ const viewController = require('../controllers/viewController')
 const router = express.Router()
 
 // GET request / to display the homepage
+/**
+ * @swagger
+ * /:
+ *  get:
+ *   summary: get all memes displayed on the homepage
+ *   description: get all memes displayed on the homepage
+ *   responses:
+ *    200:
+ *     description: success
+ *    500:
+ *     description: error
+ */
 router.get('/', viewController.getHomePage)
 
 //GET request to display add-meme screen
+/**
+ * @swagger
+ * /add-meme:
+ *  get:
+ *   summary: render form to post memes
+ *   description: render form to post memes
+ *   responses:
+ *    200:
+ *     description: success
+ *    500:
+ *     description: error
+ */
 router.get('/add-meme', viewController.getAddMemePage)
 
 module.exports = router
