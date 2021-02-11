553e168e4a8937783c2900327276503428a8e5d0
Manthan Gupta <manthangupta109@gmail.com>
2021-02-11 09:06:19 +0530

    Updated test_server.sh file

--- test_server.sh
+++ test_server.sh
@@ -5,18 +5,6 @@ chmod +x server_run.sh
 chmod +x sleep.sh
 ./sleep.sh
 curl --location --request GET 'http://localhost:8081/memes'
-curl --location --request POST 'http://<Server_URL>/memes' \
-
---header 'Content-Type: application/json' \
-
---data-raw '{
-
-"name": "xyz",
-
-"url": "abc.com",
-
-"caption": "This is a meme"
-
-}'
+curl --location --request POST 'http://localhost:8081/memes' --header 'Content-Type: application/json' --data-raw '{"name": "xyz","url": "https://static.wixstatic.com/media/bb1bd6_5798c09022ba43249a38bfea9be1db34~mv2.png/v1/fill/w_1064,h_608,al_c,q_90/bb1bd6_5798c09022ba43249a38bfea9be1db34~mv2.webp","caption": "This is a meme"}'
 curl --location --request GET 'http://localhost:8081/memes'
 curl --location --request GET 'http://localhost:8080/swagger-ui/'
