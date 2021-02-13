bf849a585e97b8b0bb7cfd1d3519e11e1d8cb427
Manthan Gupta <manthangupta109@gmail.com>
2021-02-13 13:27:17 +0530

    install script updated

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongod
 sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
-sudo systemctl status mongod
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt install npm
